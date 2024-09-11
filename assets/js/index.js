1338cd601e5ff4f7ceb24d13ce13e390c8dd2c73
Raphael Pereira de Souza <89276182+raphaelpereirarps03@users.noreply.github.com>
2024-09-10 21:27:46 -0300

    Testando Js

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,7 +19,7 @@ window.onclick = function(event) {
 
 let selecionado="false"
 function selecionado() {
-  selecionado="true";
+  let selecionado="true";
   document.querySelector(".card-item-plano").style.backgroundColor="#669D31";
   document.querySelector(".fw-normal").style.color="#FEFADC";
 }
