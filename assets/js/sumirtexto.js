20e310be07599a160be7c19a9464e74d9e0b5cc0
Wesleyr66 <wesley.rochaO@hotmail.com>
2024-09-11 16:03:51 -0300

    Páginas

---
+++ assets/js/sumirtexto.js
@@ -0,0 +1,8 @@
+function show_camada1(){
+    document.getElementById("p1").style.visibility="visible";
+    document.getElementById("p2").style.visibility="hidden";
+}
+function show_camada2(){
+    document.getElementById("p1").style.visibility="hidden";
+    document.getElementById("p2").style.visibility="visible";
+}
