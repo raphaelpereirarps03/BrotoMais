c05f1d7fef9e5120a8c294dd3b3eaa09fcafde36
wesley <wesley.rochao@hotmail.com>
2024-09-16 19:19:16 -0300

    modal,homepage patch

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -6,23 +6,33 @@ let fundo = document.querySelector("#fundo_escuro")
             compra.style.visibility="hidden"
             fundo.style.visibility="visible"
             fundo.style.transition= "opacity 0.5s";
+            document.getElementsByTagName("main")[0].style.filter = "blur(10px)";
+            document.getElementsByTagName("body")[0].style.overflow = "hidden";
             fundo.style.opacity=0.9;
         }
         function fechar_modal(){
             modal.style.visibility="hidden"
             compra.style.visibility="visible"
             fundo.style.visibility="hidden"
+            fundo.style.transition= "opacity 0.5s";
+            document.getElementsByTagName("main")[0].style.filter = "blur(0px)";
+            document.getElementsByTagName("body")[0].style.overflow = "visible";
             fundo.style.opacity = 0;
         }
         function abrir_modal2(){
             modal.style.visibility="visible"
             fundo.style.visibility="visible"
             fundo.style.transition= "opacity 0.5s";
+            document.getElementsByTagName("main")[0].style.filter = "blur(10px)";
+            document.getElementsByTagName("body")[0].style.overflow = "hidden";
             fundo.style.opacity=0.9;
         }
         function fechar_modal2(){
             modal.style.visibility="hidden"
             fundo.style.visibility="hidden"
+            fundo.style.transition= "opacity 0.5s";
+            document.getElementsByTagName("main")[0].style.filter = "blur(0px)";
+            document.getElementsByTagName("body")[0].style.overflow = "visible";
             fundo.style.opacity = 0;
         }
         
